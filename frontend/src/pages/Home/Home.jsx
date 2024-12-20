713f5ce591cf1c037b6964a366ebd0d06c130b4e
faraz vaissi <farazmrx@gmail.com>
2024-12-20 19:57:36 +0330

    Complate header part

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import './Home.css'
+import Header from '../../components/Header/Header'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+        <Header/>
+    </div>
   )
 }
 
